3b1ed9d59686e0e7f2d610623606cea186a14bca
Alioune4 <papalioune1@gmail.com>
2024-06-22 19:34:49 +0200

    refactor(sequelize): remove dead code

--- microservices/shared/database/sequelize.js
+++ microservices/shared/database/sequelize.js
@@ -6,13 +6,6 @@ const sequelize = new Sequelize('postgres://user:postgresPass@postgres:5432/js')
 
 const operation = retry.operation({ retries: 5, factor: 3, minTimeout: 5000 });
 
-/*sequelize.authenticate().then(() => {
-    console.log('Connection to postgres has been established successfully.');
-
-}).catch((error) => {
-    console.error('Unable to connect to the postgres database:', error);
-})*/
-
 operation.attempt((currentAttempt) => {
     console.log(`Connecting to database, attempt ${currentAttempt}`);
     sequelize
